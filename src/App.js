7384df62ff3ed0b79d2145da58cef0b6ae0f2775
shon <shonbwd@gmail.com>
2024-02-13 13:13:44 +0530

    crypto

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import express from "express";
-import Route from "./routes/authRoutes.js";
+import authRoute from "./routes/authRoutes.js";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import crypto from "crypto";
 
 const app = express();
 
 // middlewares
 app.use(cors());
 app.use(express.json());
-app.use(morgan(dev));
+app.use(morgan("dev"));
 app.use(cookieParser());
 
 // routes
 app.use("/api/v1/auth", authRoute);
+
+// crypto
+/*
+const key = crypto.pseudoRandomBytes(64).toString("hex");
+console.log(key);
+*/
 
 app.use("/", (req, res) => {
   res.send("<h1>Blog App</h1>");
