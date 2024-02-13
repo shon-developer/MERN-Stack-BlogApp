651c9bf306eabc8043bf874d5b419d64127bf977
shon <shonbwd@gmail.com>
2024-02-13 13:11:58 +0530

    encrypt password, userschema methods to compare passwords and get token

--- src/models/userSchema.js
+++ src/models/userSchema.js
@@ -1,4 +1,7 @@
 import mongoose from "mongoose";
+import JWT from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import config from "../config/index.js";
 
 const userSchema = new mongoose.Schema(
   {
@@ -21,5 +24,25 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+// encrypt the password
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+// userSchema methods
+userSchema.methods = {
+  // compare password
+  comparePassword: async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+  },
+  // generate JWT token
+  getJWTtoken: function () {
+    return JWT.sign({ _id: this._id }, config.JWT_SECRET, {
+      expiresIn: config.JWT_EXPIRY,
+    });
+  },
+};
 
 export default mongoose.model("User", userSchema);
